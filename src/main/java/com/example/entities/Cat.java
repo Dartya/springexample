40c8b47def3d4d0f335563f254ce90079fa40685
Александр Питасов <alexpit63@gmail.com>
2019-01-04 22:22:11 +0300

    Initial commit

---
+++ src/main/java/com/example/entities/Cat.java
@@ -0,0 +1,17 @@
+package com.example.entities;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Cat {
+
+    private String name = "Барсик";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
